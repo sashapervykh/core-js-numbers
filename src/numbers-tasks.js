ca7aa6bb660f18562f68be15f128845e2b832e67
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-11-05 12:04:35 +0300

    feat: add getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -285,8 +285,13 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; ) {
+    sum += i;
+    i += 1;
+  }
+  return sum;
 }
 
 /**
